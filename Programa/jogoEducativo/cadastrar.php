0f0c85544cd62cf19be2b668fd52f715c8f75cb2
ALESSANDRO DE OLIVEIRA LEMOS <45742267+aolCarioca@users.noreply.github.com>
2019-07-16 13:26:41 -0300

    atualização dia 16/07/2019

---
+++ Programa/jogoEducativo/cadastrar.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Jogo Educativo - Cadastrar   </title>
+    <link rel="shortcut icon" href="imagens/favicon.ico" type="image/x-png/">
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+
+</head>
+<body>
+<div id="corpo-form-Cad">
+    <h1>Cadastrar</h1>
+    <form method="POST">
+        <input type="text"     name="nome_prof" placeholder="Nome Completo" maxlength="30">
+        <input type="email"    name="email"     placeholder="Usuário"       maxlength="40">
+        <input type="password" name="senha"     placeholder="Senha"         maxlength="15">
+        <input type="password" name="confSenha"     placeholder="Confirmar Senha" maxlength="15">
+        <input type="submit"   value="Cadastrar">
+        
+    </form>
+</div>
+<?php
+
+
+?>
+</body>
+</html>
